5bd8df7ad2a28e381bcfc83d56f43e12d4aad164
Tony Barquera <barqueratony@gmail.com>
2024-03-27 13:51:01 -0600

    [UPDATE] Dynamic Guitars

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,12 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
+          { data.map(guitar => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+            />
+          )) }
         </div>
       </main>
 
